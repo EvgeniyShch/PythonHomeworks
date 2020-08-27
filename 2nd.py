0f352367765c6e4213126a6df194fb6d8d91ef7f
EvgeniyShchepelin <evgeniyginsane@gmail.com>
2020-08-28 00:33:09 +0300

    impot time

--- 2nd.py
+++ 2nd.py
@@ -7,3 +7,10 @@ seconds = userseconds%3600%60
 usertime ="{}{}:{}{}:{}{}".format(hours//10,hours%10,minutes//10,minutes%10,seconds//10,seconds%10)
 
 print(f"Введенное Вами количество {userseconds} секунд в формате ЧЧ:MM:CC равняется {usertime} ")
+
+
+import time
+#localtime = time.asctime( time.localtime(time.time()) )
+
+localtime = time.clock_gettime(userseconds)
+print ("Local current time :", localtime)
