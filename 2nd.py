d0c1df4f54edce0ca88816d7ed374ab5a37502e0
EvgeniyShchepelin <evgeniyginsane@gmail.com>
2020-08-28 00:01:20 +0300

    2 задание

---
+++ 2nd.py
@@ -0,0 +1,9 @@
+userseconds = int(input("Введите количество секунд"))
+
+hours = userseconds//3600
+minutes = userseconds%3600//60
+seconds = userseconds%3600%60
+
+usertime ="{}{}:{}{}:{}{}".format(hours//10,hours%10,minutes//10,minutes%10,seconds//10,seconds%10)
+
+print(f"Введенное Вами количество {userseconds} секунд в формате ЧЧ:MM:CC равняется {usertime} ")
