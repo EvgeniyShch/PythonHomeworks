94ee5a8521dc117b7a838ca3455fe833ced388bf
EvgeniyShchepelin <evgeniyginsane@gmail.com>
2020-08-28 14:27:31 +0300

    Задание 3
    Добавлена проверка вводных данных

--- Task3.py
+++ Task3.py
@@ -1,13 +1,20 @@
-number = int(input("Введите число"))
-lenghtofnumber = len(str(number))
-print(lenghtofnumber) # для проверки длины
+while True :
+    try :
+        number = int(input("Введите число"))
+        if  number>0:
+            break
+        else:
+            print("Введите положительное целое число")
+    except :
+        print("Введите положительное целое число")
 
 i=1
 multiplier = 10
 
-while i < lenghtofnumber :
+while i < len(str(number)) :
     i+=1
     multiplier *=10
 
 result = number*(multiplier**2) + 2*number*multiplier + 3*number
-print(f' Сумма чисел {number} и {number*multiplier+number} и {number*(multiplier**2)+number*multiplier+number} будет {result}')
+print("Результат введенного числа N будет представлен в виде суммы N + NN + NNN")
+print(f'Сумма чисел {number} и {number*multiplier+number} и {number*(multiplier**2)+number*multiplier+number} будет {result}')
