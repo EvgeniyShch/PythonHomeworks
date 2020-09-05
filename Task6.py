a334094a40124448926faabdee0b4c91d2121a78
EvgeniyShchepelin <evgeniyginsane@gmail.com>
2020-09-05 11:43:58 +0300

    Решение через функцию в функции

--- Task6.py
+++ Task6.py
@@ -18,3 +18,24 @@ under the arm and over the shoe, \
 top on the head, and out goes you"
         
 print(int_func(test_text))
+
+# Решение через функцию в функции
+
+def external_func(words):
+    
+    def internal_func(word):
+        letters = list(word)
+        letters[0] = chr(ord(letters[0])-32)
+        word =""
+        for i in letters:
+            word = word + str(i)
+        return word
+    
+    words_list = words.split(" ")
+    words =""
+    for i in range(len(words_list)):
+        words = words + internal_func(words_list[i]) + " "
+        
+    return words
+
+print(external_func(test_text))
